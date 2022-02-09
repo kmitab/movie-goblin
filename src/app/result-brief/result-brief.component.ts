be99abc68b32286ff4fd2c023dfe66d1f0869da5
kmitab <kmita.boguslaw@gmail.com>
2022-02-09 04:25:03 +0100

    fixed page navigation not resetting on new movie

--- src/app/result-brief/result-brief.component.ts
+++ src/app/result-brief/result-brief.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { PluralResult } from '../plural.result';
 import { SearchService } from '../services/search.service';
+import { SingularResult } from '../singular.result';
 
 @Component({
   selector: 'app-result-brief',
   templateUrl: './result-brief.component.html',
   styleUrls: ['./result-brief.component.css']
 })
-export class ResultBriefComponent {
+export class ResultBriefComponent implements OnInit {
 
+  oCombined$?: Observable<[SingularResult, PluralResult]>;
   pageNumber = 1;
+  currentFilmSelection?: string;
 
-  constructor(public searchService: SearchService) { }
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit() {
+    this.oCombined$ = this.searchService.oCombined$.pipe(
+      tap(result => {
+        if (this.currentFilmSelection != result[0].imdbID) {
+          console.log("film changed, resetting page navigation");
+          this.pageNumber = 1;
+        }
+        this.currentFilmSelection = result[0].imdbID;
+      })
+    );
+  }
 
   nextPage() {
     this.pageNumber++;
