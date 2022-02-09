fa2d0247fcf8cf6c379a731f5e27b9bc3d3dc760
kmitab <kmita.boguslaw@gmail.com>
2022-02-09 14:31:11 +0100

    next page button checking number of pages

--- src/app/plural.result.ts
+++ src/app/plural.result.ts
@@ -17,6 +17,13 @@ export class PluralResult {
         return false;
     }
 
+    public getNumberOfPages(): number {
+        if (this.hasValues()) {
+            return Math.ceil(Number(this.pluralResponse?.totalResults) / 10);
+        }
+        return 0;
+    }
+
     public getSearchArray(): FilmBrief[] {
         return (!this.pluralResponse || this.pluralResponse.Response == "False")
             ? [] : this.pluralResponse.Search;
