13940b00c2239709f0f89185325c4e1b220bfbe4
kmitab <kmita.boguslaw@gmail.com>
2022-02-11 16:29:38 +0100

    search.service taking/sending "type" param

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -6,6 +6,13 @@ import {
 
 import { FilmService } from './film.service';
 
+enum ResultType {
+  All = "",
+  Movie = "movie",
+  Series = "series",
+  Episode = "episode",
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,22 +23,24 @@ export class SearchService {
     distinctUntilChanged(),
     tap(n => console.log(`pageNumberOutput: ${n}`)),
   );
-  combinedChange = new Subject<{ term?: string, page?: number; }>();
+  combinedChange = new Subject<{ term?: string, page?: number, type?: ResultType; }>();
   combinedOutput$ = this.combinedChange.pipe(
     debounceTime(300),
     distinctUntilChanged(),
-    switchMap(({ term: term, page: page }) => this.search(term, page)),
+    switchMap(({ term: term, page: page, type: type }) => this.search(term, page, type)),
   );
   private currentSearchTerm?: string;
   private currentPageNumber = 1;
+  private currentResultType = ResultType.All;
 
   constructor(private filmService: FilmService) { }
 
-  private search(term?: string, page?: number) {
+  private search(term?: string, page?: number, type?: ResultType) {
 
     // use given term/page or last term/page if none given
     this.currentSearchTerm = term ?? this.currentSearchTerm;
     this.currentPageNumber = page ?? this.currentPageNumber;
+    this.currentResultType = type ?? this.currentResultType;
 
     // update Subject/Observable announcing page change
     this.pageNumberChange.next(this.currentPageNumber);
@@ -56,5 +65,10 @@ export class SearchService {
     this.currentPageNumber--;
     this.pageNumberChange.next(this.currentPageNumber);
     this.combinedChange.next({ page: this.currentPageNumber });
+  }
+
+  setResultType(resultType: ResultType) {
+    this.currentResultType = resultType;
+    this.combinedChange.next({ type: this.currentResultType });
   }
 }
